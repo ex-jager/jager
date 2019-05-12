c8a98321b33fa1c72a88ed5d5d00681bc2924a6f
Gabriel Alves <g04lves@gmail.com>
2019-05-12 18:53:38 -0300

    Refactors application module.

--- lib/jager/application.ex
+++ lib/jager/application.ex
@@ -2,9 +2,5 @@ defmodule Jager.Application do
   use Application
   alias Jager.Recorder
 
-  def start(_type, _args) do
-    children = [Recorder]
-    opts = [strategy: :one_for_one, name: Jager.Supervisor]
-    Supervisor.start_link(children, opts)
-  end
+  def start(_type, _args), do: Supervisor.start_link([Recorder], strategy: :one_for_one, name: Jager.Supervisor)
 end
